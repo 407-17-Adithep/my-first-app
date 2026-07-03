139f1dc4de3862d4faffc95509a65a2f2c275ca2
407-17-Adithep <54316@yupparaj.ac.th>
2026-07-03 11:50:49 +0700

    Update convertYear.py

--- convertYear.py
+++ convertYear.py
@@ -1,2 +1,34 @@
 import steamlit as st
 st.title("แอปพิเคชั้นแปลงปี พ.ศ. เป็น ค.ศ.")
+
+bh_year=st.number_input("กรอกปี พ.ศ. ที่ต้องการเปลี่ยนแปลง",value=2569)
+ce_year=bh_year-543
+st.header(f"ปี ค.ศ. คือ : {ce_year}")
+
+
+
+
+
+
+
+
+
+
+
+                         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                         
